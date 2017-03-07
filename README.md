51cb10113fe808c73a11bf03e586403cc0effd92
Douman <douman@gmx.se>
2017-03-07 11:06:34 +0300

    Minor bugfix, better random kana algo and double kanas

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Futsū Kana
 
+[![Build Status](https://travis-ci.org/DoumanAsh/futsu-kana.svg?branch=master)](https://travis-ci.org/DoumanAsh/futsu-kana)
+
 Simple web app to learn [kana](https://en.wikipedia.org/wiki/Kana).
 
 ## Requirements
--- app/js/UI.js
+++ app/js/UI.js
@@ -8,9 +8,18 @@ import {look_up_parent_until} from './utils.js';
  * @param {Object} kana Object with references to kana tables.
  */
 export function init_start(question, kana) {
-    const question_kanas = [];
+    let question_kanas_back = []; //Backup kanas
+    let question_kanas = [];
 
-    //Adds kanas from column if input is checked.
+    /**
+     * Add kanas from column to pool, if needed.
+     *
+     * It is expected to have empty kanas in UI, these are going to be skipped.
+     * @param {Object} column DOM div with kanas.
+     *                 First element is input that determines whether to add to pool or not.
+     *
+     * @returns {void}
+     */
     const add_column_if = (column) => {
         if (!column.children[0].children[0].checked) return;
 
@@ -26,6 +35,10 @@ export function init_start(question, kana) {
         }
     };
 
+    /**
+     * Initializes list of kanas from UI tables.
+     * @returns {void}
+     */
     const init_kanas = () => {
         //Both supposed to have equal length
         const hiragana_columns = kana.hiragana.children;
@@ -35,43 +48,84 @@ export function init_start(question, kana) {
             add_column_if(hiragana_columns[idx]);
             add_column_if(katakana_columns[idx]);
         }
+
+        question_kanas_back = question_kanas.slice(0);
     };
 
+    /**
+     * Gets random kana from remaining ones.
+     * If there is no more, then restore pool of kanas from backup.
+     *
+     * @returns {Object} Kana object or undefined if there is none.
+     */
     const random_kana = () => {
-        return question_kanas[Math.floor(Math.random() * question_kanas.length)];
+
+        if (question_kanas.length === 0) {
+            if (question_kanas_back.length === 0) return undefined;
+            else question_kanas = question_kanas_back.slice(0);
+        }
+
+        const idx = Math.floor(Math.random() * question_kanas.length);
+        return question_kanas.splice(idx, 1)[0];
     };
 
+    /**
+     * Switches to next kana question.
+     *
+     * @returns {Boolean} true if new kana is available.
+     *                    false otherwise.
+     */
     const next_kana = () => {
         let q_kana = random_kana();
 
         if (q_kana === undefined) return false;
 
-        while (q_kana.romaji === question.answer.innerHTML) q_kana = random_kana();
-
         question.answer.style.visibility = 'hidden';
         question.answer.innerHTML = q_kana.romaji;
+
+        //Change text with animation.
+        question.question.className = question.question.className.replace(/ *animate_fade_in */, '');
+        void question.question.offsetWidth;
         question.question.innerHTML = q_kana.kana;
+        question.question.className += " animate_fade_in";
 
         return true;
     };
 
+    /**
+     * Handles input changes.
+     *
+     * It verifies answer as user types its:
+     * 1. If expected and user input are the same, then move to next kana.
+     * 2. If not, then check whether user input len is equal or more of expected to indicate error.
+     * 3. Otherwise do nothing.
+     *
+     * @param {Object} event DOM event.
+     * @returns {void}
+     */
     const input_on_change = (event) => {
         const expected_text = question.answer.textContent.toLowerCase();
         const value = event.target.value.toLowerCase();
 
         if (value === expected_text) {
-            next_kana();
             event.target.value = '';
             event.target.className = event.target.className.replace(" wrong", "");
+            next_kana();
         }
         else if (value.length >= expected_text.length) {
-            event.target.className += " wrong";
+            if (!/wrong/.test(event.target.className)) {
+                event.target.className += " wrong";
+            }
         }
         else {
             event.target.className = event.target.className.replace(" wrong", "");
         }
     };
 
+    /**
+     * Event handler for hover to show answer.
+     * @returns {void}
+     */
     const question_hover = () => {
         question.answer.style.visibility = '';
     };
@@ -89,12 +143,14 @@ export function init_start(question, kana) {
         }
         else {
             question_kanas.length = 0;
+            question_kanas_back.length = 0;
             question.input.disabled = true;
 
             question.answer.innerHTML = '　';
             question.question.innerHTML = '　';
             question.answer.style.visibility = 'hidden';
 
+            question.input.className = question.input.className.replace(" wrong", "");
             question.input.removeEventListener('input', input_on_change);
             question.question.removeEventListener('mouseover', question_hover);
         }
